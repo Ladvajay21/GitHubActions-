9507bac655dc6c0cf1a26920fed94517cd5bc2a1
Ladvajay21 <jay.ladva@julesai.com>
2025-05-20 12:17:36 +0530

    1st commit for Jules

---
+++ JulesAutomation/Purchasepage.spec.js
@@ -0,0 +1,25 @@
+const { test, expect } = require('@playwright/test');
+const { clearLine } = require('readline');
+
+test('browserEvoke', async ({ page }) => {
+
+  await page.goto('https://demo.haroldwaste.com/authentication');
+
+  const Email = page.locator('[data-test-id="input-email"]');
+  const Password = page.locator('[data-test-id="input-password"]')
+  const login = page.locator('[data-test-id="signin"]');
+
+  await Email.getByRole('textbox').fill('jay.ladva@julesai.com');
+  await Password.getByRole('textbox').fill('Jay1234');
+  await login.click();
+ 
+
+
+
+
+
+
+
+
+
+})
