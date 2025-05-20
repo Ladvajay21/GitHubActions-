d9e42d2ae395cd2c8af1da3ab4798215b70e6dee
Ladvajay21 <jay.ladva@julesai.com>
2025-05-20 11:46:48 +0530

    Config

--- tests/purhasePage.spec.js
+++ tests/purhasePage.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test');
 const { clearLine } = require('readline');
-const {JulesLogin} = require('../loginConstants/JulesLogin');
+//const {JulesLogin} = require('../loginConstants/JulesLogin');
 const Email = 'jay.ladva@julesai.com';
 const password = 'Jay1234';
 
@@ -10,18 +10,18 @@ const password = 'Jay1234';
 const jules = new JulesLogin(page);
 await jules.goto();
 await jules.loginScript(Email , password);
-//await page.goto('https://demo.haroldwaste.com/authentication');
+await page.goto('https://demo.haroldwaste.com/authentication');
 
-  //const Email = page.locator('[data-test-id="input-email"]');
-  //const Password = page.locator('[data-test-id="input-password"]')
-  //const login = page.locator ('[data-test-id="signin"]');
-  //const purchases = await page.locator("//tbody/tr");
-  //const purchaseRef = "P2024-11-0-002-361" ;
+  const Email = page.locator('[data-test-id="input-email"]');
+  const Password = page.locator('[data-test-id="input-password"]')
+  const login = page.locator ('[data-test-id="signin"]');
+  const purchases = await page.locator("//tbody/tr");
+  const purchaseRef = "P2024-11-0-002-361" ;
 
-  //await Email.getByRole('textbox').fill('jay.ladva@julesai.com');
-  //await Password.getByRole('textbox').fill ('Jay1234');
-  //await login.click();
-  //const sale = await page.locator('xpath=//tbody/tr').filter({ hasText: "P2024-11-0-002-361" }).click();
+  await Email.getByRole('textbox').fill('jay.ladva@julesai.com');
+  await Password.getByRole('textbox').fill ('Jay1234');
+  await login.click();
+  const sale = await page.locator('xpath=//tbody/tr').filter({ hasText: "P2024-11-0-002-361" }).click();
   
 
 
